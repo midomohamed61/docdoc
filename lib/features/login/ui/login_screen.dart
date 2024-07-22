e7061ef6efd41295cdd3ecd72f6381c3d4145a00
your-githb-usernam <midomohamed61>
2024-07-22 10:42:28 +0300

    add app_regex , spacing and constants for future

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,10 +1,37 @@
+import 'package:docdoc/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text(
+              'Welcome Back',
+              style: TextStyles.font24BlackBold,
+            ),
+            SizedBox(
+              height: 8.h,
+            ),
+            Text(
+              'Please login to your account',
+              style: TextStyles.font14GrayRegular,
+            ),
+            SizedBox(
+              height: 8.h,
+            ),
+          ]),
+        ),
+      ),
+    ));
   }
 }
